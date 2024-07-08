92785482ffc4315384527956f96e977507aa5287
Jonas Spinner <jonasspinner@gmx.de>
2024-07-08 22:54:38 +0200

    Basic setup: Upload simulations; constants and simulation_loader; add gitignore and requirements

---
+++ utils/constants.py
@@ -0,0 +1,21 @@
+# fundamental constants
+e = 1.6e-19
+c = 3e8
+
+# masses (all in MeV)
+me = 0.51099895000
+mmu = 105.6583755
+mtau = 1776.86
+mp = 938.272088
+mn = 939.565420
+alphaEM = 1 / 137.035999084
+
+# transforming units
+erg2MeV = 6.2415e5
+invs2MeV = 1 / 1.5e24 * 1e3
+km2invMeV = 5.1e15
+MeV2invfm = 5.1e-3
+invcmtoMeV = 1 / 5.1e10
+
+# supernova-specific constants
+twoThirds = 2 / 3  # in case you dont trust astrophysics
---
+++ utils/simulation_loader.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+import utils.constants as c
+
+SIM_PATH = "simulations"
+SIM_FILES = {
+    "SFHo-18.6": "hydro-SFHo-s18.6-MUONS-T=0.99948092.txt",
+    "SFHo-18.80": "hydro-SFHo-s18.80-MUONS-T=1.0001996.txt",
+    "SFHo-20.0": "hydro-SFHo-s20.0-MUONS-T=1.0010874.txt",
+    "LS220": "hydro-LS220-T=1.0001464.txt",
+}
+
+
+def load_simulation(name):
+    file = os.path.join(SIM_PATH, SIM_FILES[name])
+    assert os.path.exists(file)
+
+    tab = np.loadtxt(file, skiprows=5, dtype=np.float64)
+
+    R = 1e-5 * tab[:, 0] * c.km2invMeV  # in MeV^-1
+    T = tab[:, 4]  # in MeV
+    mu = {
+        "e": tab[:, 9],  # in MeV
+        "mu": tab[:, 12],  # in MeV
+        "p": tab[:, 11] + c.mp,
+        "n": tab[:, 10] + c.mn,
+        "nu_e": tab[:, 8],
+        "nu_mu": tab[:, 13],
+    }
+    return R, T, mu
