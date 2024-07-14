Fixed bug in inverse trapping (missing weighting factor) and updated results; partly implemented exact trapping

LLNuChi/trapping.py
@@ -1,10 +1,15 @@
 import numpy as np
 import vegas
+from scipy.interpolate import interp1d
+from scipy.integrate import quad
 
 from utils.kinematics import get_s
 from utils.thermodynamics import get_Fdeg
 from utils.trapping import Trapper
 from LLNuChi.analytical import get_sigma
+
+GAMMA_MIN = 1e-100
+X_MAX = 1e2
 
 
 class Trapper_LLNuChi(Trapper):
@@ -15,10 +20,42 @@ class Trapper_LLNuChi(Trapper):
             imfp += self._get_imfp_inverse_scat(operator, antilepton=False, **kwargs)
             imfp += self._get_imfp_inverse_scat(operator, antilepton=True, **kwargs)
         elif self.approach == "exact":
-            raise NotImplementedError
+            imfp = self.get_imfp_exact(operator, **kwargs)
         else:
             raise ValueError(f"approach {self.approach} not implemented")
         return imfp
+
+    def _get_gamma_diff_ann(
+        self, operator, s, Echi, Enu, mL, mChi, Lambda, T, mu_nuL, Fdeg_Lm, Fdeg_Lp
+    ):
+        sigma = get_sigma(
+            "annihilation", operator, s=s, mL=mL, mChi=mChi, Lambda=Lambda
+        )
+        factor1 = 1 / (4 * np.pi * Echi) * (s - mChi**2)
+        factor2 = Enu / (np.exp((Enu - mu_nuL) / T) + 1)
+        gamma = Fdeg_Lm * Fdeg_Lp * factor1 * factor2 * sigma
+        mask1 = s > 4 * mL**2
+        mask2 = s > mChi**2
+        mask = np.logical_and(mask1, mask2)
+        gamma[~mask] = 0.0
+        return gamma
+
+    def _get_gamma_diff_scat(
+        self, operator, s, Echi, EL, mL, mChi, Lambda, T, mu_L, Fdeg_L, Fdeg_nu
+    ):
+        sigma = get_sigma("scattering", operator, s=s, mL=mL, mChi=mChi, Lambda=Lambda)
+        factor1 = (
+            1
+            / (4 * np.pi * Echi)
+            * ((s - mL**2 - mChi**2) ** 2 - (2 * mL**2 * mChi**2)) ** 0.5
+        )
+        factor2 = (EL**2 - mL**2) ** 0.5 / (np.exp((EL - mu_L) / T) + 1)
+        gamma = Fdeg_L * Fdeg_nu * factor1 * factor2 * sigma
+        mask1 = s > (mL + mChi) ** 2
+        mask2 = s > mL**2
+        mask = np.logical_and(mask1, mask2)
+        gamma[~mask] = 0.0
+        return gamma
 
     def _get_imfp_inverse_ann(self, operator, T, mu, model):
         mu_L, mu_nuL = mu["e"], mu["nu_e"]
@@ -26,26 +63,18 @@ class Trapper_LLNuChi(Trapper):
         Fdeg_Lm = get_Fdeg(mL, T, mu_L, is_boson=False)
         Fdeg_Lp = get_Fdeg(mL, T, -mu_L, is_boson=False)
 
-        def get_integrand(s, Echi, Enu):
-            sigma = get_sigma(
-                "annihilation", operator, s=s, mL=mL, mChi=mChi, Lambda=Lambda
-            )
-            factor1 = 1 / (4 * np.pi * Echi) * (s - mChi**2)
-            factor2 = Enu / (np.exp((Enu - mu_nuL) / T) + 1)
-            integrand = Fdeg_Lm * Fdeg_Lp * factor1 * factor2 * sigma
-            mask1 = s > 4 * mL**2
-            mask2 = s > mChi**2
-            mask = np.logical_and(mask1, mask2)
-            integrand[~mask] = 0.0
-            return integrand
-
         @vegas.batchintegrand
         def f(x):
             a, b, costh = x[..., 0], x[..., 1], x[..., 2]
             Echi, Enu = T * a / (1 - a), T * b / (1 - b)
             jac = T / (1 - a) ** 2 * T / (1 - b) ** 2
             s = get_s(mChi, 0.0, Echi, Enu, costh)
-            integrand = get_integrand(s, Echi, Enu)
+            gamma = self._get_gamma_diff_ann(
+                operator, s, Echi, Enu, mL, mChi, Lambda, T, mu_nuL, Fdeg_Lm, Fdeg_Lp
+            )
+            imfp = gamma / (1 - mChi**2 / Echi**2) ** 0.5
+            weight = self.mean_free_path_weight(Echi, mChi, T, is_boson=False)
+            integrand = imfp * weight
             assert np.isfinite(integrand).all()
             return integrand
 
@@ -62,7 +91,119 @@ class Trapper_LLNuChi(Trapper):
         Fdeg_L = get_Fdeg(mL, T, mu_L, is_boson=False)
         Fdeg_nu = get_Fdeg(0.0, T, mu_nuL, is_boson=False)
 
-        def get_integrand(s, Echi, EL):
+        @vegas.batchintegrand
+        def f(x):
+            a, b, costh = x[..., 0], x[..., 1], x[..., 2]
+            Echi, EL = T * a / (1 - a), T * b / (1 - b)
+            jac = T / (1 - a) ** 2 * T / (1 - b) ** 2
+            s = get_s(mChi, mL, Echi, EL, costh)
+            gamma = self._get_gamma_diff_scat(
+                operator, s, Echi, EL, mL, mChi, Lambda, T, mu_L, Fdeg_L, Fdeg_nu
+            )
+            imfp = gamma / (1 - mChi**2 / Echi**2) ** 0.5
+            weight = self.mean_free_path_weight(Echi, mChi, T, is_boson=False)
+            integrand = imfp * weight
+            assert np.isfinite(integrand).all()
+            return integrand
+
+        integrator = vegas.Integrator(
+            [[mChi / (T + mChi), 1.0], [mL / (T + mL), 1.0], [-1.0, 1.0]]
+        )
+        factor = integrator(f, nitn=self.nitn, neval=self.neval, alpha=self.alpha).mean
+        norm = self.mean_free_path_normalization(mChi, T)
+        return factor / norm
+
+    def get_imfp_exact(self, operator, T, mu, model, nsteps=100):
+        mChi = model["mChi"]
+
+        x_min = mChi / T * (1 + 1e-2)
+        Echi = np.logspace(np.log(x_min), np.log(X_MAX), nsteps) * T
+        Gamma = np.zeros(nsteps)
+        for i in range(nsteps):
+            Gamma[i] += self._get_gamma_ann(
+                operator, Echi=Echi[i], T=T, mu=mu, model=model
+            )
+            Gamma[i] += self._get_gamma_scat(
+                operator, antilepton=False, Echi=Echi[i], T=T, mu=mu, model=model
+            )
+            Gamma[i] += self._get_gamma_scat(
+                operator, antilepton=True, Echi=Echi[i], T=T, mu=mu, model=model
+            )
+        assert np.isfinite(Gamma).all()
+
+        # Gamma = 0. for small Echi?
+        print(Gamma)
+        Gamma = np.clip(Gamma, a_min=GAMMA_MIN, a_max=None)
+        print(Gamma)
+        print("1/gamma", 1 / Gamma)
+        mfp = (1 - mChi**2 / Echi**2) ** 0.5 / Gamma
+        weighting = self.mean_free_path_weight(Echi, mChi, T, is_boson=False)
+        print("weight", weighting)
+        integrand_grid = mfp * weighting
+        print("grid", integrand_grid)
+        integrand_interpolation = interp1d(
+            np.log(Echi / T), np.log(integrand_grid), kind="linear"
+        )
+        # assert np.isfinite(mfp).all()
+
+        import matplotlib.pyplot as plt
+
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.plot(Echi, weighting, label="weight")
+        # plt.plot(Echi, mfp, label="mfp")
+        plt.legend()
+        plt.show()
+
+        factor = quad(
+            lambda x: np.exp(integrand_interpolation(np.log(x))), x_min, X_MAX
+        )[0]
+        norm = self.mean_free_path_normalization(mChi, T)
+        mfp = factor / norm
+        imfp = 1 / mfp
+        print(mfp, imfp)
+        return imfp
+
+    def _get_gamma_ann(self, operator, Echi, T, mu, model):
+        mu_L, mu_nuL = mu["e"], mu["nu_e"]
+        mL, mChi, Lambda = [model[key] for key in ["mL", "mChi", "Lambda"]]
+        Fdeg_Lm = get_Fdeg(mL, T, mu_L, is_boson=False)
+        Fdeg_Lp = get_Fdeg(mL, T, -mu_L, is_boson=False)
+
+        def get_integrand(s, Enu):
+            sigma = get_sigma(
+                "annihilation", operator, s=s, mL=mL, mChi=mChi, Lambda=Lambda
+            )
+            factor1 = 1 / (4 * np.pi * Echi) * (s - mChi**2)
+            factor2 = Enu / (np.exp((Enu - mu_nuL) / T) + 1)
+            integrand = Fdeg_Lm * Fdeg_Lp * factor1 * factor2 * sigma
+            mask1 = s > 4 * mL**2
+            mask2 = s > mChi**2
+            mask = np.logical_and(mask1, mask2)
+            integrand[~mask] = 0.0
+            return integrand
+
+        @vegas.batchintegrand
+        def f(x):
+            a, costh = x[..., 0], x[..., 1]
+            Enu = T * a / (1 - a)
+            jac = T / (1 - a) ** 2
+            s = get_s(mChi, 0.0, Echi, Enu, costh)
+            integrand = get_integrand(s, Enu)
+            assert np.isfinite(integrand).all()
+            return integrand
+
+        integrator = vegas.Integrator([[0.0, 1.0], [-1.0, 1.0]])
+        Gamma = integrator(f, nitn=self.nitn, neval=self.neval, alpha=self.alpha).mean
+        return Gamma
+
+    def _get_gamma_scat(self, operator, antilepton, Echi, T, mu, model):
+        mu_L, mu_nuL = -mu["e"] if antilepton else mu["e"], mu["nu_e"]
+        mL, mChi, Lambda = [model[key] for key in ["mL", "mChi", "Lambda"]]
+        Fdeg_L = get_Fdeg(mL, T, mu_L, is_boson=False)
+        Fdeg_nu = get_Fdeg(0.0, T, mu_nuL, is_boson=False)
+
+        def get_integrand(s, EL):
             sigma = get_sigma(
                 "scattering", operator, s=s, mL=mL, mChi=mChi, Lambda=Lambda
             )
@@ -81,17 +222,14 @@ class Trapper_LLNuChi(Trapper):
 
         @vegas.batchintegrand
         def f(x):
-            a, b, costh = x[..., 0], x[..., 1], x[..., 2]
-            Echi, EL = T * a / (1 - a), T * b / (1 - b)
-            jac = T / (1 - a) ** 2 * T / (1 - b) ** 2
+            a, costh = x[..., 0], x[..., 1]
+            EL = T * a / (1 - a)
+            jac = T / (1 - a) ** 2
             s = get_s(mChi, mL, Echi, EL, costh)
-            integrand = get_integrand(s, Echi, EL)
+            integrand = get_integrand(s, EL)
             assert np.isfinite(integrand).all()
             return integrand
 
-        integrator = vegas.Integrator(
-            [[mChi / (T + mChi), 1.0], [mL / (T + mL), 1.0], [-1.0, 1.0]]
-        )
-        factor = integrator(f, nitn=self.nitn, neval=self.neval, alpha=self.alpha).mean
-        norm = self.mean_free_path_normalization(mChi, T)
-        return factor / norm
+        integrator = vegas.Integrator([[mL / (T + mL), 1.0], [-1.0, 1.0]])
+        Gamma = integrator(f, nitn=self.nitn, neval=self.neval, alpha=self.alpha).mean
+        return Gamma

utils/trapping.py
@@ -5,7 +5,7 @@ from utils.supernova import get_trapping_sphere_radius
 
 
 class Trapper:
-    def __init__(self, mfp_prescription="Rosseland", approach="exact"):
+    def __init__(self, mfp_prescription="thermal", approach="exact"):
         # TODO: LOGGER
         self.nitn = 10
         self.neval = 1000
@@ -33,6 +33,7 @@ class Trapper:
         # Note: Assume that this is the mfp of the dark particle -> use mu = 0
         x = E / T
         if self.mfp_prescription == "Rosseland":
+            # p**2 * E**2 * exp(x) * f**2
             weight1 = (1 - mass**2 / E**2) * x**4
             weight2 = np.where(
                 E / T < 1e2,
@@ -43,7 +44,10 @@ class Trapper:
             )
             return weight1 * weight2
         elif self.mfp_prescription == "thermal":
-            return 1 / (np.exp(x) - 1) if is_boson else 1 / (np.exp(x) + 1)
+            # E * p * f
+            weight1 = (1 - mass**2 / E**2) ** 0.5 * x
+            weight2 = 1 / (np.exp(x) - 1) if is_boson else 1 / (np.exp(x) + 1)
+            return weight1 * weight2
         else:
             raise ValueError(
                 f"mfp_prescription {self.mfp_prescription} not implemented"